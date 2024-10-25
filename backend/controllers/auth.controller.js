3001083e88f24488c005f0fd0dfe286853b45792
Kavindu Tharusha Nanayakkara <140879249+KavinduTharusha02@users.noreply.github.com>
2024-10-25 07:34:11 +0530

    Initialized Sign In API Route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken';  
 
 export const signup = async(req, res, next) => {
    const{username, email, password} = req.body;
@@ -15,3 +16,21 @@ const hashedPassword = bcryptjs.hashSync(password, 10);
    }
    
 };
+
+export const signin = async(req, res, next) => {
+   const {email, password} = req.body;
+   try {
+      const validUser = await User.findOne({email});
+      if(!validUser)return next(errorHandler(404, "User not Found"));
+      const isValidPassword = bcryptjs.compareSync(password, validUser.password);
+      if(!isValidPassword)return next(errorHandler(401, "wrong Credentials"));
+      const token = jwt.sign({userId: validUser._id}, process.env.JWT_SECRET);
+      const{password:hashedPassword,...rest}=validUser._doc;
+      const expiryDate = new Date(Date.now() + 3600000);
+      res.cookie("my_token", token, {httpOnly: true})
+      .status(200)
+      .json(rest);
+   } catch (error) {
+      next(error);
+   }
+}
