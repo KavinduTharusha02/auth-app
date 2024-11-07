1d9aaec5e560370a34e18590811ac1c31fae0fd9
Kavindu Tharusha Nanayakkara <140879249+KavinduTharusha02@users.noreply.github.com>
2024-11-08 02:15:18 +0530

    Updated Header Component and Make Profile Private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom"
+import { useSelector } from "react-redux" 
 
 
 function Header() {
+    const {currentUser} = useSelector((state) => state.user);
   return (
     <div className='bg-slate-200'>
 <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -15,8 +17,12 @@ function Header() {
     <Link to='/about'>
         <li>About</li>
         </Link>
-    <Link to='/sign-in'>
-        <li>Sign In</li>
+    <Link to='/profile'>
+    {currentUser ? (
+<img src = {currentUser.profilePic} alt = {currentUser.name} className='w-7 h-7 rounded-full'/>
+    ):(
+    <li>Sign In</li>
+    )}
         </Link>
     </ul>
 </div>
--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,35 +1,44 @@
-import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
-import { app } from "../firebase";
-import { useDispatch } from "react-redux";
-import { signInSuccess } from "../redux/user/userSlice.js";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
-function OAuth() {
-    const dispatch = useDispatch();
-const handleGoogleClick = async () => {
-    try{
-        const provider=new GoogleAuthProvider();
-        const auth = getAuth(app);
-        const result = await signInWithPopup(auth, provider);
-        const res = await fetch('/backend/auth/google', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: result.user.displayName,
-                email: result.user.email,
-                photo: result.user.photoURL,
-            }),
-        });
-        const data = await res.json();
-        dispatch(signInSuccess(data));
-    }catch(error) {
-        console.log('could not login with google', error);
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch('/backend/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('could not login with google', error);
     }
-};
+  };
   return (
-    <button type='button' onClick={handleGoogleClick} className='bg-gray-300 text-gray-800 font-semibold rounded-lg p-3 uppercase hover:opacity-50'>Continue With Google</button>
-  )
+    <button
+      type='button'
+      onClick={handleGoogleClick}
+      className='bg-gray-500 text-white rounded-lg p-3 uppercase hover:opacity-95'
+    >
+      Continue with google
+    </button>
+  );
 }
-
-export default OAuth;
