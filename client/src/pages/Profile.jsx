71def5c178f7c309632c726182381424a0a2b4e0
Kavindu Tharusha Nanayakkara <140879249+KavinduTharusha02@users.noreply.github.com>
2024-11-08 19:03:22 +0530

    Completed User Update Functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,39 +1,92 @@
-{/*I couldn't make upload profile picture functionality, Cz nowadays firebase storage is not frr even in spark plan*/}
-
-
 import { useSelector } from "react-redux";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { updateUserStart, updateUserSuccess, updateUserFailure, signInSuccess } from "../redux/user/userSlice";
 
 function Profile() {
+  const dispatch = useDispatch();
   const fileRef = useRef(null);
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser, loading,error } = useSelector((state) => state.user);
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`backend/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success===false){
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error))
+    }
+  };
+
   return (
-    <div className='p-3 max-w-lg mx-auto'>
-     <h1 className='text-3xl font-bold text-center my-7 '>Profile</h1>
-     <form className='flex flex-col gap-4'>
-      <input type="file" ref={fileRef} hidden accept='image/*'/>
-      <img 
-      src={currentUser.profilePic} alt="Profile" 
-      className='h-24 w-24 rounded-full self-center cursor-pointer object-cover mt-2'
-      onClick={()=>fileRef.current.click()}
-      />
-      <input 
-      defaultValue={currentUser.username} 
-      type="text" id='usrename' placeholder='Username' className='bg-slate-100 rounded-lg p-3' 
-      />
-      <input 
-      defaultValue={currentUser.email} 
-      type="text" id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3' 
-      />
-      <input type="text" id='password' placeholder='Password' className='bg-slate-100 rounded-lg p-3' />
-      <button className="bg-gray-700 text-white rounded-lg p-3 uppercase hover:opacity-80">Update</button>
-     </form>
-     <div className="flex justify-between mt-5">
-      <span className='text-red-700 font-semibold cursor-pointer'>Delete Account</span>
-      <span className='text-gray-800 font-semibold cursor-pointer'>Sign Out</span>
-     </div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-bold text-center my-7">Profile</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input type="file" ref={fileRef} hidden accept="image/*" />
+        <img
+          src={currentUser.profilePic}
+          alt="Profile"
+          className="h-24 w-24 rounded-full self-center cursor-pointer object-cover mt-2"
+          onClick={() => fileRef.current.click()}
+        />
+        <input
+          defaultValue={currentUser.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input
+          defaultValue={currentUser.email}
+          type="text"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
+        />
+        <button className="bg-gray-700 text-white rounded-lg p-3 uppercase hover:opacity-80">
+          {loading ? "Loading..." : "Update"}
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-700 font-semibold cursor-pointer">
+          Delete Account
+        </span>
+        <span className="text-gray-800 font-semibold cursor-pointer">
+          Sign Out
+        </span>
+      </div>
+      <p className='text-red-700 mt-5'>{error&& 'Something went wrong!'}</p>
+      <p className='text-green-500 mt-5'>{updateSuccess&& 'User is updated successfully!'}</p>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
